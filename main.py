391258bf436e5eb99ebcdb3289cef97fa61efa15
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2022-04-27 23:25:07 +0700

    chore: rearranged stuff

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, UploadFile
 from keras.models import load_model
 from pydantic import BaseModel
 import numpy as np
+import io
+from PIL import Image
 
 app = FastAPI()
 
@@ -16,21 +18,19 @@ class Output(BaseModel):
 @app.post('/')
 async def root(file : UploadFile): # from input spec		
 	contents = await file.read()
+	stream = io.BytesIO(contents)
+	img = Image.open(stream)
 	
 	# pipelines
 	# ---- BEGIN ----
 	# resize image
-	import io
-	from PIL import Image
-	stream = io.BytesIO(contents)
-	img = Image.open(stream)
 	img = img.resize((32,32))
-
-	# reshape image to input
-	result = np.array(img).reshape((1,32,32,1))
-	print(result.shape)
 	# ---- END ----
 	
+	# model aggregation
+	result = np.array(img).reshape((1,32,32,1))
+	print(result.shape)
+
 	# model prediction
 	prediction = model.predict(x=result).tolist()[0]
 	
